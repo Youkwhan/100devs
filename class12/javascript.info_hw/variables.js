7932632a4f8f839158393048aaa8f72aac1bef0c
Youkwhan <Youkwhan@gmail.com>
2023-11-21 12:16:50 -0500

    javascript.info variable hw

---
+++ class12/javascript.info_hw/variables.js
@@ -0,0 +1,21 @@
+// Working with variables
+// 1.
+let admin
+let name
+// 2.
+name = "John"
+// 3.
+admin = name
+// 4.
+alert(admin)
+
+// Giving the right name
+// 1.
+const ourPlanetName = "Earth"
+// 2.
+let currentUserName = "John"
+
+// Uppercase const
+/* We should make birthday uppercase because your 
+birthday is predetermined and does not ChannelMergerNode, 
+// whereas your age may change over time and should stau lowercase */
