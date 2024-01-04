9a58a61b1958e3f6d2540f09fb1967e45f8b7af1
Youkwhan <Youkwhan@gmail.com>
2024-01-03 23:08:41 -0500

    class21 stopwatch Object

--- class21/objects-zeo/js/main.js
+++ class21/objects-zeo/js/main.js
@@ -1,1 +1,27 @@
 //Create a stopwatch object that has four properties and three methods
+const stopwatch = {
+	time: 0,
+	color: "black",
+	increment: 1,
+	shape: "round",
+
+	start: function () {
+		this.interval = setInterval(
+			function () {
+				this.time -= this.increment
+				console.log(`Time: ${this.time}`)
+			}.bind(this),
+			1000
+		)
+	},
+
+	reset: function () {
+		this.time = 0
+	},
+	setTime: function (newTime) {
+		this.time = newTime
+	},
+}
+
+stopwatch.setTime(10)
+stopwatch.start()
