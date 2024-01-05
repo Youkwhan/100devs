54eef8678e33847b34af11cc973516ef90677ad4
Youkwhan <Youkwhan@gmail.com>
2024-01-05 13:19:29 -0500

    class22 space-review

--- class22/space-review/js/main.js
+++ class22/space-review/js/main.js
@@ -1,12 +1,47 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+const nums = [1, 24, 5, 346, 3, 4]
+console.log(
+	nums.reduce((total, cur) => total + cur),
+	0
+)
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
-
+function squared(nums) {
+	return nums.map((num) => num ** 2)
+}
+console.log(squared(nums))
 //Create a function that takes string
 //Print the reverse of that string to the console
-
+function reverse(str) {
+	const res = str.split("").reverse().join("")
+	// const res = str.split("").reduce((acc,cur) => cur + acc, "")
+	console.log(res)
+}
+reverse("water")
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+function palindrome(str) {
+	let left = 0,
+		right = str.length - 1
+	while (left < right) {
+		while (left < right && !/[a-zA-Z]/.test(str[left])) {
+			left++
+		}
+		while (left < right && !/[a-zA-Z]/.test(str[right])) {
+			right--
+		}
+
+		if (str[left].toLowerCase() !== str[right].toLowerCase()) {
+			return false
+		}
+		left += 1
+		right -= 1
+	}
+	return true
+}
+
+console.log(palindrome("racecar"))
+console.log(palindrome("helloleh"))
--- class22/turbo-review/js/main.js
+++ class22/turbo-review/js/main.js
@@ -2,7 +2,7 @@
 // Declare a variable and assign it to your fav drink as a string. Make sure there is no whitespace on either side of the string,
 // and print the value to the console
 let drink = "water"
-drink.trim()
+drink = drink.trim()
 console.log(drink)
 
 // Declare a variable, assign it a string of multiple words, and check to see if one of the words is "apple".
@@ -13,16 +13,16 @@ console.log(stringWords.includes("water"))
 // Create a function that returns rock, paper, or scissors as randomly as possible
 const rps = () => {
 	const randomPercentage = Math.random()
-	if (randomPercentage <= 0.3) {
+	if (randomPercentage <= 0.33) {
 		return "rock"
-	} else if (randomPercentage <= 0.6) {
+	} else if (randomPercentage <= 0.66) {
 		return "paper"
 	} else {
 		return "scissors"
 	}
 }
 
-rps()
+// rps()
 // *Conditionals*
 // Create a function that takes in a choice (rock, paper, or scissors) and
 // determines if they won a game of rock paper scissors against a bot using the above function
@@ -47,7 +47,7 @@ console.log(result)
 
 // *Loops*
 // Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array.
-//  Print the results of each game to the console.
+// Print the results of each game to the console.
 function game(arr) {
 	const n = arr.length
 	let result
