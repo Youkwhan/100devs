ec98bfc655cc051e0d00e180190a7c908af0c356
Youkwhan <Youkwhan@gmail.com>
2024-01-05 16:00:48 -0500

    class22 streetfighter

--- class22/objects-lost-galaxy/js/main.js
+++ class22/objects-lost-galaxy/js/main.js
@@ -1,1 +1,24 @@
 //Create a mouse object that has four properties and three methods
+function Mouse(brand, color, model, wireless) {
+	this.brand = brand
+	this.color = color
+	this.model = model
+	this.wireless = wireless
+
+	this.leftClick = function () {
+		console.log("left click")
+	}
+	this.rightClick = function () {
+		console.log("right click")
+	}
+	this.scroll = function () {
+		console.log("scrollll")
+	}
+}
+
+const mouse = new Mouse("logictech", "black", "Ergo", true)
+const mouse2 = new Mouse("apple", "silver", "track", true)
+
+// A prototype is another object that is used as a fallback Inheritance!
+// Mouse does not have a weight so, we check the object itself, then constructor, then it's global object prototype
+Mouse.prototype.weight = "heavy"
--- class22/objects-street-fighter/js/main.js
+++ class22/objects-street-fighter/js/main.js
@@ -1,1 +1,23 @@
-//Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+//Create a street fighter constructor that makes fighting game characters
+// with 4 properties and 3 methods
+
+function StreetFighter(name, attack, catchPhrase, specialMove) {
+	this.name = name
+	this.attack = attack
+	this.catchPhrase = catchPhrase
+	this.specialMove = specialMove
+
+	this.haduken = function () {
+		console.log("HAAADUKEN")
+	}
+
+	this.winning = function () {
+		console.log(`Show your move, ${this.catchPhrase}`)
+	}
+
+	this.taunt = function () {
+		console.log(`ultimate ${this.speicalMove}!`)
+	}
+}
+
+let ryu = new StreetFighter("ryu", "high punch", "Get over here", "HADUKEN")
