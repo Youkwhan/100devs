0369e688a07c175f2501eaa2b0c9db3b228912bd
Youkwhan <Youkwhan@gmail.com>
2024-01-11 17:15:39 -0500

    class23 and tonyHawk constructor

---
+++ class23/objects-tony-hawk-pro-skater/js/main.js
@@ -0,0 +1,18 @@
+//Create a Tony Hawk Pro Skater constructor that makes fighting game
+//  characters with 4 properties and 3 methods
+
+function Skater(skate, name, health, wheels) {
+	this.skate
+	this.name
+	this.health
+	this.wheels
+	this.attack = function () {
+		console.log("LONG board attack")
+	}
+	this.dodge = function () {
+		console.log("low duck skater")
+	}
+	this.taunt = function () {
+		console.log("do you even skate bro")
+	}
+}
