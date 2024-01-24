882201381be2e8bfc141464f1f967746fca0ff09
Youkwhan <Youkwhan@gmail.com>
2024-01-23 19:53:43 -0500

    Netflix class constructor

--- class27/objects-netflix-tv-shows/js/main.js
+++ class27/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,22 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class TVShows {
+  constructor(name, length, genre, rating) {
+    this.name = name;
+    this.length = length;
+    this.genre = genre;
+    this.rating = rating;
+  }
 
+  play() {
+    console.log("Playing show");
+  }
+
+  bookmark() {
+    console.log("Added to list");
+  }
+
+  stop() {
+    console.log("stop");
+  }
+}
