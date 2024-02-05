88750f2ab5fe3aeae367803a587b9f3351ffaee0
Youkwhan <Youkwhan@gmail.com>
2024-02-05 16:05:06 -0500

    OOP encapsulation and abstraction

--- class30/objects-espresso-machine/js/main.js
+++ class30/objects-espresso-machine/js/main.js
@@ -1,1 +1,21 @@
-//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+//Create an espresso machine class that makes machines with 4 properties and 3 methods
+class Espresso {
+  constructor(color, make, model, price) {
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.price = price;
+  }
+
+  turnOn() {
+    console.log("Good day, I am now on!");
+  }
+  steam() {
+    console.log("steaming");
+  }
+  brew() {
+    console.log("Good stuff coming your way");
+  }
+}
+
+const machine = new Espresso("red", "gaggia", "classic pro", 400);
