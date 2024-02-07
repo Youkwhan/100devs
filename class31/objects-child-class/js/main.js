bcc6993b73cf7bc4abe6accb535372a10ce9b228
Youkwhan <Youkwhan@gmail.com>
2024-02-06 20:54:04 -0500

    inheritance

--- class31/objects-child-class/js/main.js
+++ class31/objects-child-class/js/main.js
@@ -1,2 +1,29 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
 
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(`${this.name} made a loud sound!`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this.breed = breed;
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this.breed = breed;
+  }
+  speak() {
+    console.log("Meeeooowwwww");
+  }
+}
+
+let simba = new Dog("Simba", "Shepard");
