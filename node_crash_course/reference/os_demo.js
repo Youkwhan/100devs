c84e7042d8f903abb230c39ab7c796b9c5f431c8
Youkwhan <Youkwhan@gmail.com>
2024-02-13 14:41:53 -0500

    os module

---
+++ node_crash_course/reference/os_demo.js
@@ -0,0 +1,21 @@
+// information about our environment, our os
+const os = require('os');
+console.log(os.platform());
+
+// CPU Architecture
+console.log(os.arch());
+
+// CPU Core Info
+console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+// Home fir
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
